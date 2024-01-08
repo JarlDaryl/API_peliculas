e57acf91f77a7bc888ec3826a369588c19b6a364
luis <luisarquescalero@gmail.com>
2024-01-08 19:15:03 +0100

    movieById y Recent_movies hechos

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -2,7 +2,8 @@ const movieModel = require("../models/movieModel");
 
 const getMovies = async (req, res) => {
   try {
-    const data = await movieModel.find();
+    const data =
+      await movieModel.find(); /*Poniendo {}, { title: 1 } dentro del find solo  ostraria el titulo */
     res.status(200).json({ status: "succeeded", data, error: null });
   } catch (error) {
     res
@@ -11,4 +12,27 @@ const getMovies = async (req, res) => {
   }
 };
 
-module.exports = getMovies;
+const getMovieById = async (req, res) => {
+  try {
+    const movieId = req.params.id; // Obtiene el ID de los parámetros de la solicitud
+    const movie = await movieModel.findById(movieId); // Busca por su ID
+    res.status(200).json({ status: "succeeded", movie, error: null }); // Devuelve el usuario encontrado
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "failed", data: null, error: error.message }); // Maneja cualquier error que ocurra
+  }
+};
+
+const getRecentMovies = async (req, res) => {
+  try {
+    const data = await movieModel.find().sort({ createdAt: 1 }).limit(10);
+    res.status(200).json({ status: "succeeded", data, error: null });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "failed", data: null, error: error.message });
+  }
+};
+
+module.exports = { getMovies, getMovieById, getRecentMovies };
--- router/movieRoutes.js
+++ router/movieRoutes.js
@@ -1,7 +1,13 @@
-const getMovies = require("../controllers/movieControllers");
+const {
+  getMovies,
+  getMovieById,
+  getRecentMovies,
+} = require("../controllers/movieControllers");
 
 const router = require("express").Router();
 
 router.get("/movies", getMovies);
+router.get("/movies/:id", getMovieById);
+router.get("/recent_movies", getRecentMovies);
 
 module.exports = router;
