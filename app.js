35000931ee62b134d4d1cb2e5371e92ba0ee8ad8
luis <luisarquescalero@gmail.com>
2024-01-08 16:59:10 +0100

    Set Up y conexion con Mongo hecha

---
+++ app.js
@@ -0,0 +1,39 @@
+//Importamos el modulo de express
+const express = require("express");
+//Importamos MongoDB
+const mongoose = require("mongoose");
+
+//Declaramos el puerto donde se levantara el servidor
+const PORT = 3000;
+
+//esto nos permite obtener la informacion de configuracion de ".env"
+require("dotenv").config();
+
+//Inicializamos express y podemos acceder a todas las funcionalidades que nos proporciona
+const app = express();
+
+//Analizamos los archivos JSON
+app.use(express.json());
+
+const url_mongo = process.env.DATABASE_URL_DEV;
+
+//Hacemos conexion con MongoDB
+mongoose.connect(url_mongo);
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.log(`Error al conectar con mongo ${error}`);
+});
+
+db.on("connected", () => {
+  console.log(`Conecction succesfull`);
+});
+
+db.on("disconnected", () => {
+  console.log(`Conection failed`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running http://localhost:${PORT}`);
+});
